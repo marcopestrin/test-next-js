976df655b27836cfe3fb61d45e6f178b9002f69b
Marco Pestrin <pestrinmarco@gmail.com>
2021-08-10 14:09:25 -0700

    bugfix next prev user + removed error in console

--- components/detailsUser/index.js
+++ components/detailsUser/index.js
@@ -1,15 +1,25 @@
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from 'next/router'
-import { ListItemText, ListItem, List, Typography } from '@material-ui/core/';
+import Router, { useRouter } from 'next/router';
+import { ListItemText, ListItem, List, Typography, Grid, Tooltip } from '@material-ui/core/';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import { makeStyles } from '@material-ui/core/styles';
 import { getUserById } from "../../helpers/requests";
 import Loading from "../loading";
+
+const useStyles = makeStyles((theme) => ({
+    cursorPointer: {
+        cursor: "pointer"
+    }
+}));
 
 const DetailsUser = () => {
 
     const [ detailsUser, setDetailsUser ] = useState({});
     const [ spinner, setSpinner ] = useState(true);
     const router = useRouter();
+    const classes = useStyles();
 
     useEffect(( ) => {
         const { id } = router.query;
@@ -23,32 +33,63 @@ const DetailsUser = () => {
         const { code, meta, data } = await getUserById(id);
         if (code == 200) {
             setDetailsUser(data);
-            setSpinner(false);
         }
-    }
+        setSpinner(false);
+    };
 
+    const goToPrevUser = () => refreshWithNewId(JSON.parse(router.query.id) -1);
+    const goToNextUser = () => refreshWithNewId(JSON.parse(router.query.id) +1);
+
+    const refreshWithNewId = (id) => {
+        return Router.push({
+            pathname: '/details',
+            query: { id } 
+        });
+    }
+    
     return (
         <>
             { spinner ? <Loading /> : <> 
-                { detailsUser ? 
-                    <List>
-                        <ListItem divider>
-                            <ListItemText primary="Identifier" secondary={detailsUser.id} />
-                        </ListItem>
-                        <ListItem divider>
-                            <ListItemText primary="Status" secondary={detailsUser.status} />
-                        </ListItem>
-                        <ListItem divider>
-                            <ListItemText primary="Name" secondary={detailsUser.name} />
-                        </ListItem>
-                        <ListItem divider>
-                            <ListItemText primary="Email address" secondary={detailsUser.email} />
-                        </ListItem>
-                        <ListItem divider>
-                            <ListItemText primary="Gender" secondary={detailsUser.gender} />
-                        </ListItem>
-                    </List>
-                : <Typography> No Result </Typography> }
+                <Grid container spacing={3}>
+                    { detailsUser && Object.keys(detailsUser).length > 0 ? <>
+                        <Grid item xs={12}>
+                            <List>
+                                <ListItem divider>
+                                    <ListItemText primary="Identifier" secondary={detailsUser.id} />
+                                </ListItem>
+                                <ListItem divider>
+                                    <ListItemText primary="Status" secondary={detailsUser.status} />
+                                </ListItem>
+                                <ListItem divider>
+                                    <ListItemText primary="Name" secondary={detailsUser.name} />
+                                </ListItem>
+                                <ListItem divider>
+                                    <ListItemText primary="Email address" secondary={detailsUser.email} />
+                                </ListItem>
+                                <ListItem divider>
+                                    <ListItemText primary="Gender" secondary={detailsUser.gender} />
+                                </ListItem>
+                            </List>
+                        </Grid>
+                    </> : <Typography> No Result </Typography> }
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Tooltip title="Previous User" className={classes.cursorPointer}>
+                        <NavigateBeforeIcon
+                            onClick={goToPrevUser}
+                            color="primary"
+                            fontSize="large"
+                        />
+                    </Tooltip>
+                    <Tooltip title="Next User" className={classes.cursorPointer}>
+                        <NavigateNextIcon
+                            onClick={goToNextUser}
+                            color="primary"
+                            fontSize="large"
+                        />
+                    </Tooltip>
+                </Grid>
             </> }
         </>
     )
--- components/header/index.js
+++ components/header/index.js
@@ -1,20 +1,17 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { AppBar, Toolbar, Grid, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Grid, Typography, Tooltip } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+      flexGrow: 1
     },
-    icon: {
+    cursorPointer: {
         cursor: "pointer",
-        marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
+        marginRight: theme.spacing(2)
+    }
 }));
 
 const Header = () => {
@@ -22,24 +19,24 @@ const Header = () => {
     const classes = useStyles();
     
     return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <AppBar position="static">
-                        <Toolbar>
-                            {router.pathname === "/details" && (
-                                <Link href="/list">
-                                    <ArrowBackIcon className={classes.icon} />
-                                </Link>
-                            )}
-                            <Typography variant="h6" className={classes.title}>
-                                Marco Pestrin - APPLICAZIONE DI TEST
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>
-                </Grid>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <AppBar position="static">
+                    <Toolbar>
+                        {router.pathname === "/details" && (
+                            <Link href="/list">
+                                <Tooltip title="Back to user list">
+                                    <ArrowBackIcon className={classes.cursorPointer} />
+                                </Tooltip>
+                            </Link>
+                        )}
+                        <Typography>
+                            Marco Pestrin - APPLICAZIONE DI TEST
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
             </Grid>
-        </div>
+        </Grid>
     )
 }
 
